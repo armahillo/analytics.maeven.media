fbf8764c3ee1f07d1104f4ad85698a39952d86c6
Aaron Hill <armahillo@gmail.com>
2021-09-01 23:40:55 -0400

    Puma updates. Fixes config

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
-root = "#{Dir.getwd}"
+root = "/var/www/analytics.maeven.media"
 
 workers 1
 
-bind "unix://#{root}/tmp/puma/socket"
-pidfile "#{root}/tmp/puma/pid"
-state_path "#{root}/tmp/puma/state"
+bind "unix://#{root}/shared/tmp/sockets/analytics.maeven.media-puma.sock"
+pidfile "#{root}/shared/tmp/pids/puma.pid"
+state_path "#{root}/shared/tmp/states/puma.state"
 rackup "#{root}/config.ru"
 
 threads 1, 6
