1ad348b74294fa0106571710f8288b012ec7e55d
Aaron (analytics.maeven.media) <armahillo@gmail.com>
2021-09-02 02:50:21 +0000

    Final puma fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,12 @@
 root = "#{Dir.getwd}"
 
+workers 1
+
 bind "unix://#{root}/tmp/puma/socket"
 pidfile "#{root}/tmp/puma/pid"
 state_path "#{root}/tmp/puma/state"
 rackup "#{root}/config.ru"
 
-threads 4, 8
+threads 1, 6
 
 activate_control_app
